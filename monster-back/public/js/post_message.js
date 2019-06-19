be184ff07df64c8242c95c4f4bde030166207d43
preiraambroise <preira.ambroise@yahoo.fr>
2019-06-20 00:44:43 +0200

    Mise à jour coté front des messages

--- monster-back/public/js/post_message.js
+++ monster-back/public/js/post_message.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
     $('#post_message').click(function(){
         var message = $('#message_body').val();
+        $('#message_body').val('');
+        var expeditor = $('#message_expeditor').val();
+        $.ajax({
+            type: 'POST',
+            url: "/messages/new",
+            data: {
+                "message": message,
+                "expeditor": expeditor
+            }
+        });
+    });
+    $('.input_msg_write').submit(function(event){
+        event.preventDefault();
+        var message = $('#message_body').val();
+        $('#message_body').val('');
         var expeditor = $('#message_expeditor').val();
         $.ajax({
             type: 'POST',
